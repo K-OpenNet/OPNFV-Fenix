d07d3afe4fd7e842ee0e62b6f77c81f518873073
Jaewook Oh <kyle.oh95@gmail.com>
2019-11-28 02:58:07 +0000

    update

--- tacker/tosca/utils.py
+++ tacker/tosca/utils.py
@@ -168,3 +168,102 @@ def get_vdu_monitoring(template):
     if policy_dict.get('vdus'):
         monitoring_dict = policy_dict
     return monitoring_dict
+
+
+
+def get_vdu_applicationmonitoring(template):
+    tpl_temp = "topology_template"
+    n_temp = "node_templates"
+    poly = "app_monitoring_policy"
+    monitoring_dict = dict()
+    policy_dict = dict()
+    policy_dict['vdus'] = collections.OrderedDict()
+    node_list = template[tpl_temp][n_temp].keys()
+    for node in node_list:
+        nt = template[tpl_temp][n_temp][node]
+        if nt['type'] == TACKERVDU:
+            if poly in nt['properties'].keys():
+                mon_policy = nt['properties'][poly]
+                if mon_policy != 'noop':
+                    policy_dict['vdus'][node] = {}
+                    policy_dict['vdus'][node] = mon_policy
+                del template[tpl_temp][n_temp][node]['properties'][poly]
+    if policy_dict.get('vdus'):
+        monitoring_dict = policy_dict
+    return monitoring_dict
+
+
+@log.log
+def get_vdu_metadata(template, unique_id=None):
+    metadata = dict()
+    metadata.setdefault('vdus', {})
+    for nt in template.nodetemplates:
+        if nt.type_definition.is_derived_from(TACKERVDU):
+            metadata_dict = nt.get_property_value('metadata') or None
+            if metadata_dict:
+                metadata_dict['metering.server_group'] = \
+                    (metadata_dict['metering.server_group'] + '-'
+                     + unique_id)[:15]
+                metadata['vdus'][nt.name] = {}
+                metadata['vdus'][nt.name].update(metadata_dict)
+    return metadata
+
+
+@log.log
+def get_metadata_for_reservation(template, metadata):
+    """Method used to add lease_id in metadata
+
+     So that it can be used further while creating query_metadata
+
+    :param template: ToscaTemplate object
+    :param metadata: metadata dict
+    :return: dictionary contains lease_id
+    """
+
+    metadata.setdefault('reservation', {})
+    input_param_list = template.parsed_params.keys()
+    # if lease_id is passed in the parameter file,
+    # get it from template parsed_params.
+    if 'lease_id' in input_param_list:
+        metadata['reservation']['lease_id'] = template.parsed_params[
+            'lease_id']
+    else:
+        for policy in template.policies:
+            if policy.entity_tpl['type'] == constants.POLICY_RESERVATION:
+                metadata['reservation']['lease_id'] = policy.entity_tpl[
+                    'reservation']['properties']['lease_id']
+                break
+    if not uuidutils.is_uuid_like(metadata['reservation']['lease_id']):
+        raise exceptions.Invalid('Invalid UUID for lease_id')
+    return metadata
+
+
+@log.log
+def pre_process_alarm_resources(vnf, template, vdu_metadata, unique_id=None):
+    alarm_resources = dict()
+    query_metadata = dict()
+    alarm_actions = dict()
+    for policy in template.policies:
+        if policy.type_definition.is_derived_from(MONITORING):
+            query_metadata.update(_process_query_metadata(
+                vdu_metadata, policy, unique_id))
+            alarm_actions.update(_process_alarm_actions(vnf, policy))
+        if policy.type_definition.is_derived_from(RESERVATION):
+            query_metadata.update(_process_query_metadata_reservation(
+                vdu_metadata, policy))
+            alarm_actions.update(_process_alarm_actions_for_reservation(
+                vnf, policy))
+            alarm_resources['event_types'] = {
+                'start_actions': {'event_type': 'lease.event.start_lease'},
+                'before_end_actions': {
+                    'event_type': 'lease.event.before_end_lease'},
+                'end_actions': {'event_type': 'lease.event.end_lease'}}
+
+    maintenance_vdus = get_maintenance_vdus(template)
+    if maintenance_vdus:
+        alarm_actions.update(_process_alarm_actions_for_maintenance(
+            vnf, maintenance_vdus))
+
+    alarm_resources['query_metadata'] = query_metadata
+    alarm_resources['alarm_actions'] = alarm_actions
+    return alarm_resources
